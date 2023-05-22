2eb76b774f0aa007f77aa50529fb8e91ad9fec09
Raka Atmaja <rakapa23@gmail.com>
2023-05-22 12:26:36 +0700

    push

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Flutter-LoginRegister-Ui
+# Flutter Login Register UI
+## Screenshots
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+## Installation
+
+Clone the repository
+```sh
+git clone https://github.com/rakaatmaja/Flutter-LoginRegister-Ui
+```
+
+Get package or dependencies
+```sh
+flutter pub get
+```
+
+Flutter run
+```sh
+flutter run
+```
---
+++ src/lib/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const kTitle = TextStyle(
+  fontFamily: 'Poopins',
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const kLogin = TextStyle(
+  fontFamily: 'Poopins',
+  fontSize: 32,
+  fontWeight: FontWeight.bold,
+);
+
+const kHintText = TextStyle(
+    fontFamily: 'Poopins',
+    fontSize: 15,
+    color: Color.fromARGB(255, 152, 152, 152),
+    fontWeight: FontWeight.w500);
+
+const kLoginSubtitle = TextStyle(
+    fontFamily: 'Poopins',
+    fontSize: 16,
+    color: Color.fromARGB(255, 141, 141, 141));
+
+const kLoginButton = TextStyle(
+    fontFamily: 'Poopins',
+    fontSize: 16,
+    color: Color.fromARGB(255, 255, 255, 255),
+    fontWeight: FontWeight.bold);
+
+const kTitleFood = TextStyle(
+  fontFamily: 'Poopins',
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
