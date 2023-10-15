880318132d0d7cfb9fb5548b3b4bb41c2c344d11
Raka <74345491+rakaatmaja@users.noreply.github.com>
2023-10-15 21:27:29 +0700

    Update main.dart

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -10,6 +10,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
   }
